349a071b62115944ba7b84e7de7a1fc3ce9c17cb
Chinmayee Gade <96294811+Chinmayeegade@users.noreply.github.com>
2022-03-26 09:17:02 +0530

    Update apixu.service.ts

--- src/app/apixu.service.ts
+++ src/app/apixu.service.ts
@@ -10,7 +10,7 @@ export class ApixuService {
 
   getWeather(location : any) {
     return this.http.get(
-      "http://api.weatherstack.com/current?access_key=b95f2497c0b0daf667dc9508cdd96aff&query=" + location
+      "http://api.weatherstack.com/current?access_key=[YOUR_API_KEY]&query=" + location
     );
   }
 }
